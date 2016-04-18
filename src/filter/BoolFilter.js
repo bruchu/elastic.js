9c7d07ad5b1378a75dfe44023acb409c9228b156
Bruce Chu <bruce@lever.co>
2016-04-18 10:36:08 -0700

    implement shud as an alias to should

--- src/filter/BoolFilter.js
+++ src/filter/BoolFilter.js
@@ -1,10 +1,10 @@
   /**
     @class
     <p>A <code>BoolFilter</code> allows you to build <em>Boolean</em> filter constructs
-    from individual filters. Similar in concept to Boolean query, except that 
-    the clauses are other filters. Can be placed within queries that accept a 
+    from individual filters. Similar in concept to Boolean query, except that
+    the clauses are other filters. Can be placed within queries that accept a
     filter.
-  
+
     @name ejs.BoolFilter
     @ejs filter
     @borrows ejs.FilterMixin.name as name
@@ -24,10 +24,51 @@
       _common = ejs.FilterMixin('bool'),
       filter = _common.toJSON();
 
+
+      /**
+             Adds filter to boolean container. Given filter "should" appear in
+             matching documents. If passed a single Filter it is added to
+             the list of existing filters.  If passed an array of Filters,
+             they replace all existing filters.
+
+             @member ejs.BoolFilter
+             @param {(Filter|Filter[])} oFilter A valid Filter or array of
+                Filter objects.
+             @returns {Object} returns <code>this</code> so that calls can be chained.
+             */
+    function shouldFunction(oFilter) {
+      var i, len;
+
+      if (filter.bool.should == null) {
+        filter.bool.should = [];
+      }
+
+      if (oFilter == null) {
+        return filter.bool.should;
+      }
+
+      if (isFilter(oFilter)) {
+        filter.bool.should.push(oFilter.toJSON());
+      } else if (isArray(oFilter)) {
+        filter.bool.should = [];
+        for (i = 0, len = oFilter.length; i < len; i++) {
+          if (!isFilter(oFilter[i])) {
+            throw new TypeError('Argument must be an array of Filters');
+          }
+
+          filter.bool.should.push(oFilter[i].toJSON());
+        }
+      } else {
+        throw new TypeError('Argument must be a Filter or array of Filters');
+      }
+
+      return this;
+    }
+
     return extend(_common, {
 
       /**
-             Adds filter to boolean container. Given filter "must" appear in 
+             Adds filter to boolean container. Given filter "must" appear in
              matching documents.  If passed a single Filter it is added to the
              list of existing filters.  If passed an array of Filters, they
              replace all existing filters.
@@ -39,11 +80,11 @@
              */
       must: function (oFilter) {
         var i, len;
-        
+
         if (filter.bool.must == null) {
           filter.bool.must = [];
         }
-    
+
         if (oFilter == null) {
           return filter.bool.must;
         }
@@ -56,20 +97,20 @@
             if (!isFilter(oFilter[i])) {
               throw new TypeError('Argument must be an array of Filters');
             }
-            
+
             filter.bool.must.push(oFilter[i].toJSON());
           }
         } else {
           throw new TypeError('Argument must be a Filter or array of Filters');
         }
-        
+
         return this;
       },
 
       /**
-             Adds filter to boolean container. Given filter "must not" appear 
-             in matching documents. If passed a single Filter it is added to 
-             the list of existing filters.  If passed an array of Filters, 
+             Adds filter to boolean container. Given filter "must not" appear
+             in matching documents. If passed a single Filter it is added to
+             the list of existing filters.  If passed an array of Filters,
              they replace all existing filters.
 
              @member ejs.BoolFilter
@@ -79,7 +120,7 @@
              */
       mustNot: function (oFilter) {
         var i, len;
-        
+
         if (filter.bool.must_not == null) {
           filter.bool.must_not = [];
         }
@@ -87,7 +128,7 @@
         if (oFilter == null) {
           return filter.bool.must_not;
         }
-    
+
         if (isFilter(oFilter)) {
           filter.bool.must_not.push(oFilter.toJSON());
         } else if (isArray(oFilter)) {
@@ -96,20 +137,20 @@
             if (!isFilter(oFilter[i])) {
               throw new TypeError('Argument must be an array of Filters');
             }
-            
+
             filter.bool.must_not.push(oFilter[i].toJSON());
           }
         } else {
           throw new TypeError('Argument must be a Filter or array of Filters');
         }
-        
+
         return this;
       },
 
       /**
-             Adds filter to boolean container. Given filter "should" appear in 
-             matching documents. If passed a single Filter it is added to 
-             the list of existing filters.  If passed an array of Filters, 
+             Adds filter to boolean container. Given filter "should" appear in
+             matching documents. If passed a single Filter it is added to
+             the list of existing filters.  If passed an array of Filters,
              they replace all existing filters.
 
              @member ejs.BoolFilter
@@ -117,34 +158,8 @@
                 Filter objects.
              @returns {Object} returns <code>this</code> so that calls can be chained.
              */
-      should: function (oFilter) {
-        var i, len;
-        
-        if (filter.bool.should == null) {
-          filter.bool.should = [];
-        }
 
-        if (oFilter == null) {
-          return filter.bool.should;
-        }
-    
-        if (isFilter(oFilter)) {
-          filter.bool.should.push(oFilter.toJSON());
-        } else if (isArray(oFilter)) {
-          filter.bool.should = [];
-          for (i = 0, len = oFilter.length; i < len; i++) {
-            if (!isFilter(oFilter[i])) {
-              throw new TypeError('Argument must be an array of Filters');
-            }
-            
-            filter.bool.should.push(oFilter[i].toJSON());
-          }
-        } else {
-          throw new TypeError('Argument must be a Filter or array of Filters');
-        }
-        
-        return this;
-      }
-      
+      should: shouldFunction,
+      shud: shouldFunction
     });
   };
